299d492715470aeba666b975c1ab8b3b8b74494b
Gyan <gyanendrapatro02@gmail.com>
2020-11-13 12:40:47 +0530

    welcome page

--- src/Home.js
+++ src/Home.js
@@ -3,27 +3,44 @@ import { ReactComponent as WelcomeImg } from "./tst.svg";
 
 const Home = () => {
   return (
-    <div className="h-100 container-fluid d-flex align-items-center">
-        <div className="col-1"></div>
-        <div className="col-4"><WelcomeImg /></div>
-        <div className="col-1"></div>
-        <div className="col-5">
+    <div className="h-100 container-fluid d-sm-flex align-items-center">
+        <div className="col-0 col-sm-1"></div>
+        <div className="col-12 col-sm-4 wel-img"><WelcomeImg /></div>
+        <div className="col-0 col-sm-1"></div>
+        <div className="col-12 col-sm-5 wel-header">
           <h3 style={{ color: "#808080" }}>Welcome to Get Going !!!</h3>
             <h4 style={{ color: "#444444" }}>Your Fitness Our Care</h4>
             <p>
             Here you will get to learn about sports and fitness, collaborate with others in the world,
             get rewarded recognized for the hard work.
             </p>
-            <div className="d-flex">
-              <div >
-                <button>Sports</button>
-              </div>
-                <div className="pl-3">
-                  <button >fitness</button>
+            <p>
+            Here you will get to learn about sports and fitness, collaborate with others in the world,
+            get rewarded recognized for the hard work.
+            </p>
+            <div className="screening-buttons d-sm-block">
+                  <div className="d-flex screening-buttons-width screening-button-mobile">
+                    {/* <Link to={allConditionsNotAgree ? '/attestationSuccess' : '/attestationFailure'}> */}
+                    <button
+                      type="button"
+                      id=""
+                      className="btn w-50 btn-primary text-truncate"
+                      onClick={()=>{}}
+                    >
+                      Sports
+                    </button>
+                    <button
+                      type="button"
+                      id=""
+                      className="btn screening-cancel-button w-50 ml-2 btn-secondary text-truncate"
+                      onClick={()=>{}}
+                    >
+                      Fitness
+                    </button>
+                  </div>
                 </div>
-            </div>
         </div>
-        <div className="col-1"></div>
+        <div className="col-0 col-sm-1"></div>
     </div>
   );
 };
--- src/Sports.js
+++ src/Sports.js
@@ -1,10 +1,14 @@
 const Sports = () => {
     return (
-      <h2>
-      Sports
-  
-    </h2>
+      <div className="card" style="width:400px">
+        <img className="card-img-top" src="img_avatar1.png" alt="Card image"/>
+        <div className="card-body">
+          <h4 className="card-title">John Doe</h4>
+          <p className="card-text">Some example text.</p>
+          <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
     );
   };
-  
+
   export default Sports;
