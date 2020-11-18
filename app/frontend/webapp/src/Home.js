ffc4c9d705ad72c784f49792782899a65acca69d
Gyan <gyanendrapatro02@gmail.com>
2020-11-18 20:02:40 +0530

    adding the home page fix

--- app/frontend/webapp/src/Home.js
+++ app/frontend/webapp/src/Home.js
@@ -1,36 +1,52 @@
-import React from "react";
+import React, {useHi} from "react";
 import {Container, Row, Col} from "react-bootstrap";
-import {ReactComponent as MastHeadIcon} from "./sports.svg";
+import {ReactComponent as WelcomeImg} from "./tst.svg";
+import { useHistory } from 'react-router-dom';
+import './App.css';
+
 
 const Home = () => {
+  const history = useHistory();
     return (
         <div>
-            <div className="vertical-center-container">
-                <Container>
-                    <Row>
-                        <Col sm={4}>
-                            <MastHeadIcon height={250} width={250}/>
-                        </Col>
-                        <Col sm={8}>
-                            <h3 style={{color: "#808080"}}>Get Going !!!</h3>
-                            <h4 style={{color: "#444444"}}>Lets be Fit Together</h4>
-                            <p>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting
-                                industry. Lorem Ipsum has been the industry's standard dummy text
-                                ever since the 1500s, when an unknown printer took a galley of
-                                type and scrambled it to make a type specimen book. It has
-                                survived not only five centuries, but also the leap into
-                                electronic typesetting, remaining essentially unchanged.
-                            </p>
-                            <p>
-                                It was popularised in the 1960s with the release of Letraset
-                                sheets containing Lorem Ipsum passages, and more recently with
-                                desktop publishing software like Aldus PageMaker including
-                                versions of Lorem Ipsum.
-                            </p>
-                        </Col>
-                    </Row>
-                </Container>
+            <div className="d-sm-flex vertical-center-container">
+            <div className="col-0 col-sm-1"></div>
+          <div className="col-12 col-sm-4 wel-img"><WelcomeImg /></div>
+          <div className="col-0 col-sm-1"></div>
+          <div className="col-12 col-sm-5 wel-header">
+            <h3 style={{ color: "#808080" }}>Welcome to Get Going !!!</h3>
+              <h4 style={{ color: "#444444" }}>Your Fitness Our Care</h4>
+              <p>
+              Here you will get to learn about sports and fitness, collaborate with others in the world,
+              get rewarded recognized for the hard work.
+              </p>
+              <p>
+              Here you will get to learn about sports and fitness, collaborate with others in the world,
+              get rewarded recognized for the hard work.
+              </p>
+              <div className="screening-buttons d-sm-block">
+                    <div className="d-flex screening-buttons-width screening-button-mobile">
+                      {/* <Link to={allConditionsNotAgree ? '/attestationSuccess' : '/attestationFailure'}> */}
+                      <button
+                        type="button"
+                        id=""
+                        className="btn w-50 btn-primary text-truncate"
+                        onClick={()=>history.push('/sports')}
+                      >
+                        Sports
+                      </button>
+                      <button
+                        type="button"
+                        id=""
+                        className="btn screening-cancel-button w-50 ml-2 btn-secondary text-truncate"
+                        onClick={()=>history.push('/fitness')}
+                      >
+                        Fitness
+                      </button>
+                    </div>
+                  </div>
+          </div>
+          <div className="col-0 col-sm-1"></div>
             </div>
         </div>
     );
