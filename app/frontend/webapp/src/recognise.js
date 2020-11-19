7c1120871713a77bb318a3f13f0c5db545b639fd
Dream-Sapna <74150511+Dream-Sapna@users.noreply.github.com>
2020-11-19 14:53:14 +0530

    adding new files

--- app/frontend/webapp/src/recognise.js
+++ app/frontend/webapp/src/recognise.js
@@ -1,84 +1,76 @@
-import React, {useState} from 'react';
-import {Button, Container, Row,Col,Alert, ButtonGroup, ButtonToolbar} from "react-bootstrap";
-import Image from 'react-bootstrap/Image'
+import React, { useState } from 'react';
+import { Button, Container, Row, Col, Alert, ButtonGroup, ButtonToolbar } from "react-bootstrap";
+import { ReactComponent as Certificate1 } from "./Certifiacte1.svg";
+import { ReactComponent as Certificate2 } from "./Certifiacte2.svg";
 import ShareModal from './smodal.js'
 
 const Recognise = () => {
-        
-        const [showSModal, setShowSModal] = useState(false)
-        return (
-            <Container fluid>
-              <Row>
-                  <Col sm={2}>
-                  <h3> Recognition</h3>               
-                  </Col>
-                  <Col sm ={6} ></Col>
-                  <Col sm={4}>
-                  <ButtonToolbar aria-label="Toolbar with button groups">
-                  <ButtonGroup aria-label="First Group">
-                    <Button
-                    type="button"
-                    id=""
-                    className="btn screening-cancel-button w-50 ml-2 btn-secondary"
-                    variant="secondary"
-                    >
-                      My DashBoard</Button>
-                    </ButtonGroup> 
 
-                    <ButtonGroup aria-label="Second Group ">
-                    <Button
-                    type="button"
-                    id=""
-                    className="btn screening-cancel-button w-50 ml-2 btn-secondary"
-                    variant="secondary"
-                    >
-                      Go To events</Button>
-                        </ButtonGroup> 
-                        </ButtonToolbar>      
-                  </Col>
-                  </Row>  
-                   <Row>
-                   <Col>
-                   <Alert variant="success">
-                  <Alert.Heading></Alert.Heading>
-                    <p>You have been awarded with the following certificates(Claim/Share your certificates to inspire your friends)
+  const [showSModal, setShowSModal] = useState(false)
+  return (
+    <Container fluid>
+      <Row>
+        <Col sm={2}>
+          <h3> Recognition</h3>
+        </Col>
+        <Col sm={6} ></Col>
+        <Col sm={4}>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Alert variant="success">
+            <Alert.Heading></Alert.Heading>
+            <p>You have been awarded with the following certificates(Claim/Share your certificates to inspire your friends)
                     </p>
-                     </Alert>
-                   </Col>
-                       </Row>
-                       <Row>
-                       <Col sm ={1}> </Col>
-                       <Col sm={5}>
-                       <Image src="hyz.svg/100px250" alt= "Here are your certificates" fluid/>g
-                       </Col>
-                       <Col sm ={1} ></Col>
-                        <Col sm ={5}>
-                        <Image src="hyz.svg/100px250" alt= "Here are your certificates" fluid/> 
-                        </Col>
-                       </Row>
-                       <Row>
-                       <Col sm ={1}> </Col>  
-                       <Col sm={12}>
-                         <Button variant="primary" size="lg" block>
-                            CLAIM YOUR CERTIFICATES!!!
+          </Alert>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={1}> </Col>
+        <Col sm={5}>
+          <Certificate1 height={400} width={400} />
+          <Button variant="primary" size="lg" block>
+            CLAIM YOUR CERTIFICATES!!!
                          </Button>
-                  <Button 
-                  onClick={() => setShowSModal(true)}
-                  style={{ marginRight: 15 }}
-                  variant="secondary"
-                  size="lg"
-                  block
-                  >
-                      SHARE WITH OTHERS!!!
+          <Button
+            onClick={() => setShowSModal(true)}
+            style={{ marginRight: 15 }}
+            variant="secondary"
+            size="lg"
+            block
+          >
+            SHARE WITH OTHERS!!!
                    </Button>
-                       </Col>
-                       </Row>             
-                       {showSModal && <ShareModal showModal={showSModal} showModalCallback={(status) => setShowSModal(status)}/>}
-                 
+        </Col>
+        
+        <Col sm={5}>
+          <Certificate2 height={400} width={400} />
+          <Button variant="primary" size="lg" block>
+            CLAIM YOUR CERTIFICATES!!!
+                         </Button>
+          <Button
+            onClick={() => setShowSModal(true)}
+            style={{ marginRight: 15 }}
+            variant="secondary"
+            size="lg"
+            block
+          >
+            SHARE WITH OTHERS!!!
+                   </Button>
+        </Col>
+        <Col sm={1} ></Col>
+      </Row>
+      <Row>
+        <Col sm={1}> </Col>
+
+      </Row>
+      {showSModal && <ShareModal showModal={showSModal} showModalCallback={(status) => setShowSModal(status)} />}
 
 
-              </Container>
-            );
+
+    </Container>
+  );
 };
 
 export default Recognise
